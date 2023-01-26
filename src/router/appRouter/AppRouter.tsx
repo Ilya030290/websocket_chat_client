c10d56f82c0145b7768bc3131bf69485265b70c7
Ilya030290 <anoshko90@mail.ru>
2023-01-26 18:12:54 +0300

    feat: add NotFound and SignUp pages to routes

--- src/router/appRouter/AppRouter.tsx
+++ src/router/appRouter/AppRouter.tsx
@@ -4,15 +4,17 @@ import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from '../routes';
 import Layout from '../../components/Layout';
 import Login from '../../pages/login/Login';
+import SignUp from '../../pages/signup/SignUp';
+import NotFound from '../../pages/notFound/NotFound';
 
 const AppRouter = () => (
   <Routes>
     <Route path={ROUTES.MAIN_PAGE} element={<Layout />}>
       {/*<Route index element={<MainPage />} />*/}
-      {/*<Route path={ROUTES.SIGNUP} element={<SignUp />} />*/}
-      {/*<Route path={ROUTES.NOT_FOUND} element={<NotFound />} />*/}
     </Route>
     <Route path={ROUTES.LOGIN} element={<Login />} />
+    <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+    <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
   </Routes>
 );
 
