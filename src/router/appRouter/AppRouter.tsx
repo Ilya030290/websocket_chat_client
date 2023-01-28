6e15b99cf0fdf0fd0163252245eabb58b9e87d66
Ilya030290 <anoshko90@mail.ru>
2023-01-28 13:51:12 +0300

    feat: add routes for chat and main pages

--- src/router/appRouter/AppRouter.tsx
+++ src/router/appRouter/AppRouter.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { ROUTES } from '../routes';
-import Layout from '../../components/Layout';
+import Layout from '../../common/components/Layout';
+import MainPage from '../../pages/main/MainPage';
+import ChatPage from '../../pages/chatPage/ChatPage';
 import Login from '../../pages/login/Login';
 import SignUp from '../../pages/signup/SignUp';
 import NotFound from '../../pages/notFound/NotFound';
@@ -10,7 +12,8 @@ import NotFound from '../../pages/notFound/NotFound';
 const AppRouter = () => (
   <Routes>
     <Route path={ROUTES.MAIN_PAGE} element={<Layout />}>
-      {/*<Route index element={<MainPage />} />*/}
+      <Route index element={<MainPage />} />
+      <Route path={ROUTES.CHAT_PAGE} element={<ChatPage />} />
     </Route>
     <Route path={ROUTES.LOGIN} element={<Login />} />
     <Route path={ROUTES.SIGNUP} element={<SignUp />} />
